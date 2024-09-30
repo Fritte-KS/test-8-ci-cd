f75af1c922b769f8f9b2d72bda9f6be7b4ae4809
Kristoffer Sundin <79krisun@skola.boras.se>
2024-09-30 13:30:43 +0200

    Applied command npx prettier --write .

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -1,6 +1,6 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../screens/HomeScreen";
-import DetailsScreen from "../screens/DetailsScreen";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
 
 export type RootStackParamList = {
   Home: undefined;
