3df2f58248cd51be06f9d975546e73370f4877d7
Kristoffer Sundin <79krisun@skola.boras.se>
2024-09-30 13:03:52 +0200

    Adds navigation

---
+++ navigators/RootStackNavigator.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import DetailsScreen from "../screens/DetailsScreen";
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootStackNavigator() {
+  return (
+    <RootStack.Navigator>
+      <RootStack.Screen name="Home" component={HomeScreen} />
+      <RootStack.Screen name="Details" component={DetailsScreen} />
+    </RootStack.Navigator>
+  );
+}
